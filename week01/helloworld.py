e6bd3db0c8b463e23868fc69875100dd0b4f7251
Marcin Kaminski <marcin442228@gmail.com>
2025-04-26 16:54:24 +0100

    update to helloworld.py

--- week01/helloworld.py
+++ week01/helloworld.py
@@ -1,1 +1,8 @@
+# helloworld.py
+# Author: Marcin Kaminski
+# This program prints "Hello World" to the console.
+
 print("Hello World")
+# Referenced from [1]: https://www.w3schools.com/python/python_intro.asp
+
+# End
