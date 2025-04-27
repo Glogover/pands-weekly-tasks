6dea1e91476b2e1ae714ffefad035ab0b07eb9b9
Marcin Kaminski <marcin442228@gmail.com>
2025-04-27 15:14:16 +0100

    update to collatz.py

--- week04/collatz.py
+++ week04/collatz.py
@@ -13,17 +13,30 @@ This program ends when the current value is one. """
 number = int(input("Please enter a positive integer: ")) 
 # This code snippet reads in the user-suppplied
 # number and converts it into an integer. 
+# Referenced from [6]: https://www.w3schools.com/python/python_user_input.asp
 
 while number != 1: # The while loop will run as long as the value of the positive integer is not equal to 1.
-
+# Referenced from [7]: https://www.w3schools.com/python/python_while_loops.asp
+    
     print(number, end=" ")
     # This code snippet prints the number and the argument "end=" adds a space after the printed value 
     #instead of going to a new line."
+    # Referenced from [8]: https://stackoverflow.com/questions/63569902/whats-the-meaning-of-print-end-in-python
+
 
     if number % 2 == 0: # If the number is even,
         number = number // 2 # then the number is divided by 2 (integer division i.e. without a remainder).
     else:               # If he number is odd,
         number = number * 3 + 1 # then the number is multiplied by 3 and then 1 is aded to the result.
+    # Referenced from [9]: https://365datascience.com/question/please-explain-if-x-2-0/
 
 print(number) # Finally, when the number becomes 1, it will be printed.
 
+
+# This code is a simple implementation of the Collatz conjecture,
+# which is a conjecture in mathematics that states that no matter what positive integer you start with,
+# you will always eventually reach 1 by following the above rules.
+
+# Referenced from [10]: https://en.wikipedia.org/wiki/Collatz_conjecture
+
+# End
