95df98ad8ab113585592a71edda1dfe2ada1f3d4
Marcin Kaminski <marcin442228@gmail.com>
2025-04-27 16:19:24 +0100

    update to weekday.py

--- week05/weekday.py
+++ week05/weekday.py
@@ -3,10 +3,13 @@
 # This program outputs whether or not today is a weekday. 
 
 import datetime # Importing datetime module
+# Referenced from [11]: https://docs.python.org/3.12/library/datetime.html
 
 current_date= datetime.datetime.today()  # Getting current date
+# Referenced from [12]: https://www.w3schools.com/python/python_datetime.asp
 
 day_of_the_week_integer = current_date.weekday() # Getting weekday number
+# Referenced from [12]: https://www.w3schools.com/python/python_datetime.asp
 
 """ 
 weekday() function returns the integer mapping corresponding to the specified day
@@ -28,3 +31,6 @@ if day_of_the_week_integer == 5 or day_of_the_week_integer == 6:
     print("It is the weekend, yay!")
 else: # Monday to Friday are considered a weekday
     print("Yes, unfortunately today is a weekday.")
+# Referenced from [13]: https://realpython.com/python-conditional-statements/
+
+# End
